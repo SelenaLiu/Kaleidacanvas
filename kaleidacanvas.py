58d53a0784397c3bc29192db2349a7dae2380ce5
Selena Liu <selenaliu@Selenas-MacBook.local>
2019-06-16 13:30:19 -0700

    Save and clear feature added

--- kaleidacanvas.py
+++ kaleidacanvas.py
@@ -1,13 +1,16 @@
 from Tkinter import *
+import PIL
+from PIL import Image, ImageDraw
 
 def paint(event):
 	global lastx, lasty
+	global canvas_width, canvas_height
 	x, y = event.x, event.y
 	
-	opp_lastx = 800 - lastx
-	opp_x = 800 - x
-	opp_lasty = 800 - lasty
-	opp_y = 800 - y
+	opp_lastx = canvas_width - lastx
+	opp_x = canvas_height - x
+	opp_lasty = canvas_width - lasty
+	opp_y = canvas_height - y
 	cv.create_line(lastx, lasty, x, y, fill="black")
 	cv.create_line(opp_lastx, lasty, opp_x, y, fill="black")
 	cv.create_line(opp_lastx, opp_lasty, opp_x, opp_y, fill="black")
@@ -18,6 +21,16 @@ def paint(event):
 	cv.create_line(opp_lasty, opp_lastx, opp_y, opp_x, fill="black")
 	cv.create_line(opp_lasty, lastx, opp_y, x, fill="black")
 
+	draw.line((lastx, lasty, x, y), fill="black", width=1)
+	draw.line((opp_lastx, lasty, opp_x, y), fill="black", width=1)
+	draw.line((opp_lastx, opp_lasty, opp_x, opp_y), fill="black", width=1)
+	draw.line((lastx, opp_lasty, x, opp_y), fill="black", width=1)
+
+	draw.line((lasty, lastx, y, x), fill="black", width=1)
+	draw.line((lasty, opp_lastx, y, opp_x), fill="black", width=1)
+	draw.line((opp_lasty, opp_lastx, opp_y, opp_x), fill="black", width=1)
+	draw.line((opp_lasty, lastx, opp_y, x), fill="black", width=1)
+
 	lastx, lasty = x, y
 
 def start_paint(event):
@@ -25,16 +38,37 @@ def start_paint(event):
 	lastx, lasty = event.x, event.y
 	cv.bind("<B1-Motion>", paint)
 
+def save():
+	global image_number
+	image_number += 1
+	filename = "Eclipsis" + str(image_number) + ".jpg"
+	image.save(filename)
+
+def clear():
+	cv.delete("all")
 
 master = Tk()
 master.title("Eclipsis")
 
-canvas_width = 800
-canvas_height = 800
+canvas_width = 500
+canvas_height = 500
+
+image_number = 0
+
+# constructs new image to save later
+image = PIL.Image.new('RGB', (500, 500), 'white')
+# allows user to draw on this image (above the widget in layers)
+draw = ImageDraw.Draw(image)
 
 cv = Canvas(master, width = canvas_width, height = canvas_height)
-cv.pack(expand = YES, fill = BOTH)
 cv.bind("<Button-1>", start_paint)
+cv.pack(expand = YES, fill = BOTH)
+
+
+save_button = Button(text = "Save", command = save, padx=4, pady=2)
+save_button.pack(side=LEFT)
+clear_button = Button(text = "Clear", command = clear)
+clear_button.pack(side=RIGHT)
 
 mainloop()
 
